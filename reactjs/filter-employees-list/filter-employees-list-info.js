07c07c1ee59c4c87aab31c8444a0c957f39ed9c7
JuanGrajales <inbox1.juan+github@gmail.com>
2020-08-31 06:29:23 -0400

    reactjs filter challenge complete

---
+++ reactjs/filter-employees-list/filter-employees-list-info.js
@@ -0,0 +1,4 @@
+// Link: https://www.hackerrank.com/challenges/react-filtered-employee/problem?isFullScreen=true
+// GitHub repo: https://git-rba.hackerrank.com/git/043ecc4b-00a1-4eba-b323-54690cb11ee2
+
+// Solution
